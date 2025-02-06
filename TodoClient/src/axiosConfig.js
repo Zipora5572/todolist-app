8aa03b8f3ed67ea7440be2fe80067d2f815d8226
Zipora5572 <z0548545572@gmail.com>
2025-02-06 12:09:28 +0200

    fix

--- TodoClient/src/axiosConfig.js
+++ TodoClient/src/axiosConfig.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL:  process.env.REACT_APP_API_URL, 
-  baseURL: 'http://localhost:5139',  
+   baseURL:  process.env.REACT_APP_API_URL, 
+  
   
 });
 
